d638e63d8c5a94fa2b0a3bd5176187462a9fdf5d
Marcuss17 <eduardomm171@gmail.com>
2019-09-11 07:56:03 -0500

    Adding main.cpp and CMakeLists.txt

---
+++ cmake-build-debug/CMakeFiles/LinkdList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinkdList.dir/main.cpp.obj"
+  "LinkdList.pdb"
+  "LinkdList.exe"
+  "LinkdList.exe.manifest"
+  "libLinkdList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LinkdList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
